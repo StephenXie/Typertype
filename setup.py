ca3594d99ce8df96c14da350ae80dba275ed508a
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-05-21 21:05:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 import re
 import os
 
-def get_version(package):
-    with open(os.path.join(package, "__init__.py")) as f:
-        return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
+# def get_version(package):
+#     with open(os.path.join(package, "__init__.py")) as f:
+#         return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
 
 def get_long_description():
     with open("README.md", encoding="utf8") as f:
